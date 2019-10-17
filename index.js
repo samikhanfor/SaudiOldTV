5dc0a7178e5c54367d755c0ba33a6339136daab0
samikhanfor <samogi@gmail.com>
2019-10-17 10:13:35 +0300

    first upload

---
+++ index.js
@@ -0,0 +1,26 @@
+
+
+function tick() {
+	var myAudio=document.getElementById("myAudio");
+	var date = new Date();
+	var seconds = date.getSeconds();
+	var minutes = date.getMinutes();
+	var hours = date.getHours();
+	var day = date.getDate();
+	   
+   if (myAudio.paused & seconds==1 ) return myAudio.play().catch(function() {console.log("not playing");});
+   
+   var secAngle = seconds * 6;
+   var minAngle = minutes * 6 + seconds * (360/3600);
+   var hourAngle = hours * 30 + minutes * (360/720);
+   
+   
+   $('.sec-hand').css('transform', 'rotate(' + secAngle + 'deg)');
+   $('.min-hand').css('transform', 'rotate(' + minAngle + 'deg)');
+   $('.hour-hand').css('transform', 'rotate(' + hourAngle + 'deg)');
+   $('.date').text(day);
+}
+
+
+
+setInterval(tick, 100);
