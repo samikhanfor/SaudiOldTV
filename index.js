1d0673eaae203d2e037dbc689c79201a14b9db51
Sami khanfor <samogi@gmail.com>
2019-10-24 11:04:12 +0300

    return hours move depend on minutes

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ function tick() {
    
    var secAngle = seconds * 6;
    var minAngle = minutes * 6 ;
-   var hourAngle = hours * 30 ;
+   var hourAngle = hours * 30 + minutes * (360/720) ;
    
    
    $('.sec-hand').css('transform', 'rotate(' + secAngle + 'deg)');
